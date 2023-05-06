7ea92a8f42020789971a7012cbafe4129ceceb99
sarava338 <sarava338@gmail.com>
2023-05-07 04:47:17 +0530

    Add: Created speach to text functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [recognizedText, setRecognizedText] = useState("");
+
+  const speachToText = () => {
+    const SpeechRecognition =
+      window.SpeechRecognition || webkitSpeechRecognition;
+    // const SpeechGrammarList =
+    // window.SpeechGrammarList || webkitSpeechGrammarList;
+    const SpeechRecognitionEvent =
+      window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+    const recognition = new SpeechRecognition();
+    // const speechRecognitionList = new SpeechGrammarList();
+
+    // recognition.grammars = speechRecognitionList;
+    recognition.continuous = false;
+    recognition.lang = "en-US";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    recognition.start();
+    recognition.onresult = (e) => {
+      const speachToText = e.results[0][0].transcript;
+      setRecognizedText(speachToText);
+    };
+  };
+
+  useEffect(() => {
+    // speachToText();
+    addEventListener("click", speachToText);
+  });
 
   return (
     <>
       <h1>chatBOT</h1>
+      <p>Recognized Text: {recognizedText}</p>
     </>
   );
 }
-
 export default App;
